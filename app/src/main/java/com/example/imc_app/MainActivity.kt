6b6c23fda505179829d580ca483ab93b605cfbf0
Gisele <gisesonia@gmail.com>
2021-02-12 22:56:06 -0300

    Calculo do IMC

--- app/src/main/java/com/example/imc_app/MainActivity.kt
+++ app/src/main/java/com/example/imc_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        alturaEDT
+        alturaEDT?.doAfterTextChanged { text ->
+
+        }
+
+        pesoEDT?.doAfterTextChanged { text ->
+
+        }
+
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if(peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC %.2f".format(imc)
+        }
     }
 }
